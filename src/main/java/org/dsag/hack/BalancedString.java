a014ee7d47b659df5fd0c66df2c71d7e9498906f
djhelbert <djhelbert@gmail.com>
2023-09-21 21:50:37 -0700

    Added Hacks

---
+++ src/main/java/org/dsag/hack/BalancedString.java
@@ -0,0 +1,43 @@
+package org.dsag.hack;
+
+import java.util.Stack;
+
+/**
+ * Balance characters ()[]{}
+ */
+public class BalancedString {
+
+    public static boolean isBalanced(String data) {
+        char[] chars = data.toCharArray();
+        Stack<Character> s = new Stack<>();
+
+        for(char c : chars) {
+            if(c == '(' || c == '{' || c == '[') {
+                s.push(c);
+            } else {
+                Character temp = s.pop();
+                if(temp != '(' && c == ')') {
+                    return false;
+                }
+                if(temp != '[' && c == ']') {
+                    return false;
+                }
+                if(temp != '{' && c == '}') {
+                    return false;
+                }
+            }
+        }
+
+        if(s.size() > 0) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(BalancedString.isBalanced("()[]()"));   // true
+        System.out.println(BalancedString.isBalanced("(()[]())")); // true
+        System.out.println(BalancedString.isBalanced("((("));      // false
+    }
+}
---
+++ src/main/java/org/dsag/hack/SlidingWindow.java
@@ -0,0 +1,43 @@
+package org.dsag.hack;
+
+/**
+ * Window Sliding Technique is a computational technique that aims to reduce the
+ * use of nested loops and replace it with a single loop, thereby reducing the time complexity.
+ */
+public class SlidingWindow {
+    static int maxSum(int data[], int k) {
+        int n = data.length; // Length of array
+
+        // n must be greater
+        if (n < k) {
+            System.out.println("Invalid");
+            return -1;
+        }
+
+        // Compute sum of first window of size k
+        int maxSum = 0;
+
+        for (int i = 0; i < k; i++) {
+            maxSum += data[i];
+        }
+
+        // Compute sums of remaining windows by removing first element of previous
+        // window and adding last element of current window.
+        int windowSum = maxSum;
+
+        for (int i = k; i < n; i++) {
+            windowSum += data[i] - data[i - k];
+
+            maxSum = Math.max(maxSum, windowSum);
+        }
+
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 4, 2, 10, 2, 3, 1, 0, 20};
+        int k = 4;
+
+        System.out.println(maxSum(arr, k)); // 24
+    }
+}
